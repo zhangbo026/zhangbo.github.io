f16f632116f34e7b05c8b3dad60888d927691372
zhangbo026 <18840917203@163.com>
2017-05-24 18:57:18 +0800

    音频

---
+++ 音频/data.js
@@ -0,0 +1,46 @@
+
+var audioList = [
+				{
+					"title":"后会无期",
+					"author":"邓紫棋",
+					"src":"audio/后会无期.mp3",
+					"poster":"images/head.png",
+					"duration":"3:43"
+				},
+				{
+					"title":"轮回之境",
+					"author":"阿房宫",
+					"src":"audio/轮回之境.mp3",
+					"poster":"images/albumpic_1.jpg",
+					"duration":"3:15"
+				},
+				{
+					"title":"love",
+					"author":"abc",
+					"src":"audio/love.mp3",
+					"poster":"images/albumpic_2.jpg",
+					"duration":"3:42"
+				},
+				{
+					"title":"aLIEz",
+					"author":"mizuki",
+					"src":"audio/mizuki - aLIEz.mp3",
+					"poster":"images/head.png",
+					"duration":"4:28"
+				},
+				{
+					"title":"一剪梅",
+					"author":"费玉清",
+					"src":"audio/费玉清 - 一剪梅.mp3",
+					"poster":"images/albumpic_1.jpg",
+					"duration":"3:46"
+				},
+				{
+					"title":"南山南",
+					"author":"马頔",
+					"src":"audio/马頔 - 南山南.mp3",
+					"poster":"images/albumpic_2.jpg",
+					"duration":"5:24"
+				}
+			]
+
